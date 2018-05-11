4652ccf61d5b8c3d6077c5d96726ee1c582bf928
Domantas Lankutis <domantas.lankutis@ada.ac.uk>
2018-05-11 11:22:40 +0100

    added a debug function

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 /// <reference path="./lib/p5.global-mode.d.ts"/>
 
 let spriteSheet,
-    isGenerated = false;
+    isGenerated = false,
+    debugMode = false,
+    fr = 20;
 
-function debug(player) {
+function debug() {
+    stroke(10,220,10);
+    frameRate(fr);
     for (let collision of staticCollisions) {
+        if (collision.col !== undefined) {
+            fill (collision.col);
+        }else {
+            fill(0, 50);
+        }
         rect(collision.x, collision.y, collision.width, collision.height);
     }
     rect(player.pos.x, player.pos.y, player.size, player.size);
@@ -28,6 +37,8 @@ function draw() {
         clear();
         drawMap();
         player.move(staticCollisions);
-        // debug(player);
+    }
+    if (debugMode) {
+        debug();
     }
 }
